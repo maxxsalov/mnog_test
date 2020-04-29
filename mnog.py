1039fff56cf77e65ede66f9968063a3642d99b93
maxxsalov <salovmaxx@gmail.com>
2020-04-29 15:38:49 +0300

    homework Salov

--- mnog.py
+++ mnog.py
@@ -12,3 +12,14 @@ live_near = {'john', 'vladislav', 'olga', 'mike', 'grant', 'covid', 'bilbo' }
 
 #  д/з объединить множество людей, которые живут и работают рядом
 # вывести множество людей, которые и владеют авто нужной марки, и живут и работают рядом
+people_who_live_n_work = live_near | work_near
+print("И живут и работают рядом", end=' ')
+print(people_who_live_n_work)
+
+work_n_live_n_shevrole = shevrole_owner & people_who_live_n_work
+print("И владеют авто, и живут и работают рядом", end=' ')
+print(work_n_live_n_shevrole)
+
+killer = shevrole_owner - live_near - work_near
+print("Убийца", end=' ')
+print(killer)
